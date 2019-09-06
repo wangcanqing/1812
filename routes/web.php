7890582ff7c641bde0146aa085d4c692f8829a7e
wangcanqing <2974654616@qq.com>
2019-09-06 02:20:36 +0000

    用户注册登录

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
 });
 Route::get('/user/add','UserController@add');
 Route::get('/user/redisTest','UserController@redisTest');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
